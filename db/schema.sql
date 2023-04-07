57945dc37fb7e0c3f37663aba57fb2436560e988
fardinatabassum <fardinatabassum@gmail.com>
2023-04-07 03:52:39 -0400

    added commenting to all files

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,19 @@
+--  Drops the department_db if it exists already
 DROP DATABASE IF EXISTS department_db;
+
+-- Creates the department_db database
 CREATE DATABASE department_db;
 
+-- Uses the department_db database so that following tables are creating in this database
 USE department_db;
 
+-- Creates the department table inside the department databse with the following attributes
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
+-- Creates the role table inside the department databse with the following attributes
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
@@ -18,6 +24,7 @@ CREATE TABLE role (
   ON DELETE SET NULL
 );
 
+-- Creates the employee table inside the department databse with the following attributes
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
